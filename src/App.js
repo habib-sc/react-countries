e159d4b6bfb9bf581d804026f0093d1a1f1c9472
habib-sc <habib97.cs@gmail.com>
2022-03-16 20:56:54 +0600

    Created a country component and fetch country data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <LoadCountries></LoadCountries>
+    </div>
+  );
+}
+
+function LoadCountries() {
+  const [countries, setCountries] = useState([]);
+  useEffect( () => {
+    fetch('https://restcountries.com/v3.1/all')
+    .then(res => res.json())
+    .then(data => setCountries(data));
+  } , []);
+
+  return (
+    <div className='country'>
+        <h1>Country List</h1>
+        <h2>Available Country: {countries.length}</h2>
     </div>
   );
 }
